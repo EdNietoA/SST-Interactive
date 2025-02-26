c76d48218b13a5d2c785b5214c64484d60695fef
EdNietoA <nieto.joseph@gmail.com>
2025-02-25 23:17:18 -0500

    Add files via upload

---
+++ game.js
@@ -0,0 +1,7 @@
+function checkAnswer(isCorrect) {
+    if (isCorrect) {
+        alert("¡Correcto! Levantar objetos pesados doblándose por las rodillas es más seguro.");
+    } else {
+        alert("Incorrecto. Levantar objetos pesados doblándose por la cintura puede causar lesiones.");
+    }
+}
